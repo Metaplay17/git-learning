09b215e597a72f60cfac680e2bfc5770b9d09ca1
Maxim <danin35@yandex.ru>
2026-05-23 16:17:31 +0300

    Add main code

--- App.java
+++ App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Option 1 UPDATED");
         System.out.println("NEW LINE");
+        System.out.println("MAIN CODE");
     }
 }
